56a1f0356b738470e9378938465121abfb797dd2
Mandymali <2587418667@qq.com>
2017-11-17 14:17:49 +0800

    urlerror

---
+++ URLError异常处理/jieguo.py
@@ -0,0 +1,16 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: D:/利大163 終淵早/try-except.py ==================
+[Errno 10060] 
+>>> 
+================= RESTART: D:/利大163 終淵早/try-except1.py =================
+502
+Bad Gateway
+>>> 
+=================== RESTART: D:/利大163 終淵早/URLError.py ===================
+OK
+>>> 
+=================== RESTART: D:/利大163 終淵早/hasattr.py ===================
+OK
+>>> 
